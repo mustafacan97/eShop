fbcafcafb2108f91fef0c994e06e83562d2a32a0
aksoymustafacn <aksoy.mustafacn@gmail.com>
2022-05-18 08:53:45 +0300

    add Identity packages

---
+++ src/Microservices/IdentityService/Contexts/ApplicationIdentityDbContext.cs
@@ -0,0 +1,13 @@
+﻿using IdentityService.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityService.Contexts
+{
+    public class ApplicationIdentityDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationIdentityDbContext(DbContextOptions<ApplicationIdentityDbContext> options) : base(options)
+        {
+        }
+    }
+}
--- src/Microservices/IdentityService/Program.cs
+++ src/Microservices/IdentityService/Program.cs
@@ -1,8 +1,21 @@
+using IdentityService.Contexts;
+using IdentityService.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+var _connectionString = builder.Configuration.GetConnectionString("IdentityConnection");
+
+builder.Services
+    .AddDbContext<ApplicationIdentityDbContext>(opt => opt.UseMySql(_connectionString, ServerVersion.AutoDetect(_connectionString)))
+    .AddIdentity<AppUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationIdentityDbContext>()
+    .AddDefaultTokenProviders();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,6 +30,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
